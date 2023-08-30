927c7ddeab5c95a57c54e7893fa209c23f238c55
Superdj13 <pedrosacarlos2002@gmail.com>
2023-08-30 17:08:27 +0100

    Create Evaluation_test.py
    GPT evaluates itself

---
+++ Evaluation/Evaluation_test.py
@@ -0,0 +1,41 @@
+examples = []
+question = ""
+answer = ""
+Q = False
+with open("examples.txt", "r") as f:
+ for line in f:
+   if line.lower() == "question\n":
+       Q = True
+       if question != "":
+           examples.append({"query":question,"answer":anwer})
+       question = ""
+       answer = ""
+       continue
+   if line.lower() == "answer\n":
+       Q = False
+       continue
+   if Q:
+       question += line
+   else:
+       answer += line
+ f.close()
+examples.append({"query":question,"answer":answer})
+for dictionary in examples:
+ dictionary["context"] = "You are a Large Language Model that can use a database and evaluate this question in Correctness and compare it with the Reference Answer. The answer can be CORRECT, PARTIALLY CORRECT and INCORRECT, when compared with the reference."
+
+from langchain.evaluation.loading import load_evaluator
+def results_model(model,examples): # FUNCTION TO RETAIN THE ANSWERS OF A GIVEN MODEL TO THE QUESTIONS
+  results = []
+  for example in examples:
+   results.append({"result":model( messages = [HumanMessage(content = example["query"]) ] ).content})
+  return results
+
+evaluator_context = load_evaluator("context_qa",llm = model1)
+evaluator = load_evaluator("qa",llm = model1)  ## GPT-3.5-turbo evaluators
+
+
+results = resuls_model(model1,examples)
+print(  evaluator_context.evaluate(examples,results)  )
+## WILL OUTPUT CHATGPT EVALUATION OF ITSELF
+
+
